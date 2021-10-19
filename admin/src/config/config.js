08be4933d739bd9de3be4925bba05908abc2853f
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-19 14:00:00 -0300

    sada

---
+++ admin/src/config/config.js
@@ -0,0 +1,16 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyCk1ZtWs17sJek5RDb9q3lR9W3_955rjqg",
+
+  authDomain: "konecta-c6c83.firebaseapp.com",
+
+  databaseURL: "https://konecta-c6c83-default-rtdb.firebaseio.com",
+
+  projectId: "konecta-c6c83",
+
+  storageBucket: "konecta-c6c83.appspot.com",
+
+  messagingSenderId: "700282207106",
+
+  appId: "1:700282207106:web:f4b372ca47ea2f61e02d9b"
+
+};
